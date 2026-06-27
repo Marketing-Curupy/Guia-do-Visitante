fbab5ba3f0b89f65e230e89af66ca95ba0b30e43
Marketing-Curupy <marketing@grupocurupy.com.br>
2026-06-27 09:11:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -690,7 +690,6 @@ const AJUDA = [
 
 Caso a pulseira seja retirada, rompida ou danificada, ela perderá a validade e não poderá ser reutilizada. Nessa situação, será necessário adquirir um novo ingresso para acessar o parque.`
 }
-];
 
 function renderizarAjuda() {
   const container = $("#ajudaContainer");
