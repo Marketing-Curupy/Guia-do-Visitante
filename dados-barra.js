f3179ef8322857ca4e412d5a02c44f4931a2c53e
Marketing-Curupy <marketing@grupocurupy.com.br>
2026-06-27 09:27:39 -0400

    Update dados-barra.js

--- dados-barra.js
+++ dados-barra.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   ingressosOnline: "ingressos/index.html",
- googleMaps: "https://www.google.com/maps/dir/?api=1&destination=-11.8015771,-55.4722897&travelmode=driving",
+  googleMaps: "https://www.google.com/maps/dir/?api=1&destination=-11.8015771,-55.4722897&travelmode=driving",
   whatsappGeral: "https://wa.me/556630153214"
 };
 
@@ -21,27 +21,5 @@ const VALORES_BILHETERIA = {
     visitante: 124,
     kids: 60,
     convidadoSocio: 78
-  }
-};
-
-const HORARIOS_FUNCIONAMENTO = {
-  semana: {
-    horario: "09h às 17h30",
-    classe: "dia-semana"
-  },
-
-  fimDeSemana: {
-    horario: "08h30 às 17h30",
-    classe: "dia-fim-semana"
-  },
-
-  feriado: {
-    horario: "08h30 às 17h30",
-    classe: "dia-feriado"
-  },
-
-  fechado: {
-    horario: "Fechado",
-    classe: "dia-fechado"
   }
 };
